a4f8951d1483eb329f738c393565e52caca1b1e1
kazanefu <kazanefu12@gmail.com>
2026-03-09 15:05:01 +0900

    change load to include_bytes!

--- crates/words_lib/src/save/mod.rs
+++ crates/words_lib/src/save/mod.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize,Serialize};
+use std::path::Path;
 // ==========================
 // Save / Load
 // ==========================
@@ -14,16 +15,21 @@ impl SavedEmbeddings{
     pub fn new(words:Vec<String>,embeddings: Vec<Vec<f32>>)-> Self{
         Self { words, embeddings }
     }
-    pub fn load(path: &str)->Result<Self,std::io::Error>{
+    pub fn load(path: impl AsRef<Path>)->Result<Self,std::io::Error>{
+        let path = path.as_ref();
         let bytes = std::fs::read(path)?;
         Ok(bincode::deserialize(&bytes).expect("fail to deserialize"))
     }
-    pub fn save(&self,path: &str){
+    pub fn save(&self,path: impl AsRef<Path>){
+        let path = path.as_ref();
         save_embeddings(path, self);
+    }
+    pub fn from_bytes(bytes:&[u8])->Result<Self,bincode::Error>{
+        bincode::deserialize(bytes)
     }
 }
 
-fn save_embeddings(path: &str, data: &SavedEmbeddings) {
+fn save_embeddings(path: &Path, data: &SavedEmbeddings) {
     let encoded = bincode::serialize(data).unwrap();
     std::fs::write(path, encoded).unwrap();
 }
