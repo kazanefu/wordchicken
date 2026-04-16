242dae3fa00d7d2ae188802e0ff1a148c380c572
kazanefu <kazanefu12@gmail.com>
2026-04-17 01:50:55 +0900

    add file result.rs

--- crates/game/src/main.rs
+++ crates/game/src/main.rs
@@ -4,6 +4,7 @@ mod util;
 mod start;
 mod set_target;
 mod guess;
+mod result;
 
 static WORDS_CSV: &str = include_str!("words.csv");
 static EMBEDDINGS_BYTES: &[u8] = include_bytes!("../../../assets/embedding.bin");
